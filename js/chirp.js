2ecd47dfc21e630e2d20c7c5957a3acfd1ccc34d
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-08 16:51:45 -1000

    removed extra call of chirp function

--- js/chirp.js
+++ js/chirp.js
@@ -1,13 +1,12 @@
-function chirp(n){
+function chirp(n) {
   // FIX ME
   if (n === 0) {
     return '';
   }
-  
-  chirp(--n);
-  return 'chirp ' + chirp(n);
+
+  return 'chirp ' + chirp(--n);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
   $("#result").html(chirp(3));
 });
